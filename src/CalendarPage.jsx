f3993aa94dba87908d7760ad54490a5bb911a7d3
Jan-Luca Klees <email@janlucaklees.de>
2018-12-21 17:52:41 +0100

    More general component for displaying one calendar page
    The PageGroundFloor component was adapted to serve as a more general 
    component that can accept any number of options and display them 
    correctly. With this, the API changed, so all the texts and labels are 
    now set inside the Month component. There is a new CalendarPageOption 
    component that makes adding options easiy.
    This also means, we no longer need two very similar components, reducing 
    the code duplication.

---
+++ src/CalendarPage.jsx
@@ -0,0 +1,107 @@
+import React from 'react';
+import injectSheet from 'react-jss';
+import { translate } from 'react-i18next';
+import moment from  'moment';
+
+import Page from './page.jsx';
+import Day from './day.jsx';
+
+import styles from './styles.js';
+
+
+class CalendarPage extends React.PureComponent {
+  constructor( props ) {
+    super( props );
+    this.date = moment( [ this.props.year, this.props.monthIndex ] );
+    this.numberOfOptions = this.props.children.length;
+  }
+
+  render() {
+    let { classes, t } = this.props;
+
+    // generate cells for each option one
+    let cells = this.props.children.map( () => {
+      return <td className={ classes.cell }></td>;
+    } );
+
+    // create days
+    let days = new Array();
+    for( let day = 1; day <= this.date.daysInMonth(); day++) {
+      // make a copy of the months date object and set the correct day number on it.
+      let date = this.date.clone();
+      date.date( day );
+      days.push(
+        <Day date={ date } key={ date.format( 'YYYY-MM-DD' ) }>
+          { cells }
+        </Day>
+      );
+    }
+
+    return (
+      <Page>
+        <table className={ classes.table }>
+          <thead>
+            <tr>
+              <td className={ classes.colDate }></td>
+              <td className={ classes.colDayName }></td>
+            </tr>
+            <tr>
+              <th className={ classes.rowMonthHeader } colSpan={ 2 + this.numberOfOptions }>
+                <h1 className={ classes.monthHeader }>
+                  <span className={ classes.monthNumber }>
+                    { this.date.format( '.MM.YYYY' ) }
+                  </span>
+                  <span className={ classes.monthName }>
+                    { t( 'month_names.' + this.date.month() ) }
+                    { this.date.format( ' YYYY' ) }
+                  </span>
+                </h1>
+              </th>
+            </tr>
+            <tr className={ classes.rowHeader }>
+              <th className={ classes.floorLabel } colSpan={2} >
+                <h2 className={ classes.optionLabel }>
+                  { t( 'floors.ground.abbrev_label' ) }
+                </h2>
+              </th>
+              { this.props.children }
+            </tr>
+          </thead>
+          <tbody>
+            { days }
+          </tbody>
+        </table>
+      </Page>
+    );
+  }
+}
+
+export default translate()( injectSheet( styles )( CalendarPage ) );
+
+
+
+const calendarPageOptionSyle = {
+  cell: {
+    textAlign: 'center',
+    verticalAlign: 'bottom',
+    paddingBottom: '1mm',
+  },
+  label: {
+    fontSize: '4.5mm',
+  },
+}
+class _CalendarPageOption extends React.PureComponent {
+  render() {
+    let { label, description, classes } = this.props;
+    return (
+      <th className={ classes.cell }>
+        <h2 className={ classes.label }>{ label }</h2>
+        <span>{ description }</span>
+      </th>
+    )
+  }
+}
+const CalendarPageOption = injectSheet( calendarPageOptionSyle )( _CalendarPageOption );
+
+
+export { CalendarPageOption };
--- src/month.jsx
+++ src/month.jsx
@@ -1,17 +1,49 @@
 import React from 'react';
+import { translate } from 'react-i18next';
 
-import PageGroundFloor from './page-groud-floor.jsx';
-import PageFirstFloor from './page-first-floor.jsx';
+import CalendarPage, { CalendarPageOption } from './CalendarPage.jsx';
 
-export default class Month extends React.PureComponent {
+
+class Month extends React.PureComponent {
   render() {
-    let { year, monthIndex } = this.props;
+    let { t, year, monthIndex } = this.props;
 
     return (
       <div className="month">
-        <PageGroundFloor year={ year } monthIndex={ monthIndex } />
-        <PageFirstFloor year={ year } monthIndex={ monthIndex } />
+
+        <CalendarPage year={ year } monthIndex={ monthIndex } label={ t( 'floors.ground.abbrev_label' ) }>
+          <CalendarPageOption
+            label={       t( 'floors.ground.options.at_forenoon.label' ) }
+            description={ t( 'floors.ground.options.at_forenoon.description' ) } />
+          <CalendarPageOption
+            label={       t( 'floors.ground.options.at_noon.label' ) }
+            description={ t( 'floors.ground.options.at_noon.description' ) } />
+          <CalendarPageOption
+            label={       t( 'floors.ground.options.at_afternoon.label' ) }
+            description={ t( 'floors.ground.options.at_afternoon.description' ) } />
+          <CalendarPageOption
+            label={       t( 'floors.ground.options.at_evening.label' ) }
+            description={ t( 'floors.ground.options.at_evening.description' ) } />
+          <CalendarPageOption
+            label={       t( 'floors.ground.options.at_night.label' ) }
+            description={ t( 'floors.ground.options.at_night.description' ) } />
+        </CalendarPage>
+
+        <CalendarPage year={ year } monthIndex={ monthIndex } label={ t( 'floors.first.abbrev_label' ) }>
+          <CalendarPageOption
+            label={       t( 'floors.first.options.guest_room_1.label' ) }
+            description={ t( 'floors.first.options.guest_room_1.description' ) } />
+          <CalendarPageOption
+            label={       t( 'floors.first.options.guest_room_2.label' ) }
+            description={ t( 'floors.first.options.guest_room_2.description' ) } />
+          <CalendarPageOption
+            label={       t( 'floors.first.options.creative_room.label' ) }
+            description={ t( 'floors.first.options.creative_room.description' ) } />
+        </CalendarPage>
+
       </div>
     );
   }
 }
+
+export default translate()( Month );
