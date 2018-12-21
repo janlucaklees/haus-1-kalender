e041db8eaa641e7669f3a9658c8fab40ccc02325
Jan-Luca Klees <email@janlucaklees.de>
2018-12-21 16:54:36 +0100

    Installed momentjs
    With this commit I installed momentjs to make working with dates easier. 
    Especially getting the number of dates in a month is already implemented 
    there, so I don't need to worry about making my own implementation.

--- src/page-groud-floor.jsx
+++ src/page-groud-floor.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import injectSheet from 'react-jss';
 import { translate } from 'react-i18next';
+import moment from  'moment';
 
 import Page from './page.jsx';
 import Day from './day.jsx';
@@ -9,19 +10,17 @@ import styles from './styles.js';
 
 
 class PageGroundFloor extends React.PureComponent {
+  constructor( props ) {
+    super( props );
+    this.date = moment( [ this.props.year, this.props.monthIndex ] );
+  }
+
   render() {
     let { classes, t, year, monthIndex } = this.props;
 
-    // creating the date for this month, using 0 as day number to get to the last day.
-    // Because we use 0 as value for the day, we need to increement the month by one,
-    // otherwise we will end up getting a date for the previous month.
-    // We do all this, so we can find the amount of days in the month
-    let date = new Date( year, monthIndex +1, 0 );
-    let days_in_month = date.getDate();
-
     // create days
     let days = new Array();
-    for( let day = 1; day <= days_in_month; day++) {
+    for( let day = 1; day <= this.date.daysInMonth(); day++) {
       days.push(
         <Day year={ year } monthIndex={ monthIndex } day={ day } key={ monthIndex + '-' + day }>
           <td className={ classes.cell }></td>
