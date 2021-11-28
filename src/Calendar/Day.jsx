38d82e912c3104f6472feda376a03a19eff95940
Jan-Luca Klees <email@janlucaklees.de>
2021-11-28 16:09:12 +0100

    Refactored Day component
    Here, I refactored the Day component. It is now separated into two components:
    The Day component for handling the business logic and the Row component for
    displaying.
    
    Doing so, I also implemented the logic to check whether a day is a placeholder
    or not and act accordingly.
    
    I also threw out the background handling for Firefox. It appears to work now
    without using images as background for the table cells. I think this is much
    cleaner and easier to understand.
    Support for IE is dropped with that.

--- src/Calendar/Day.jsx
+++ src/Calendar/Day.jsx
@@ -3,99 +3,48 @@ import PropTypes from 'prop-types';
 import { createUseStyles } from 'react-jss'
 import { useTranslation } from 'react-i18next';
 import moment from  'moment';
-import classNames from 'classnames';
 
-import Background from '../Print/Background.jsx';
+import DateRow from "../Table/Row";
 
 
-const weekdayBackgroundColor = [
-	'#eff0f2',
-	'#ffffff',
-	'#ffffff',
-	'#ffffff',
-	'#ffffff',
-	'#ffffff',
-	'#f7f8fb',
+const weekdayStyle = [
+	DateRow.styles.gray,
+	DateRow.styles.white,
+	DateRow.styles.white,
+	DateRow.styles.white,
+	DateRow.styles.white,
+	DateRow.styles.white,
+	DateRow.styles.lightgray
 ];
 
 export const useStyles = createUseStyles({
-	rowDay: {
-		fontSize: `4.4rem`,
-		backgroundColor: '#fff',
-	},
-	sunday:   { backgroundColor: weekdayBackgroundColor[ 0 ] },
-	saturday: { backgroundColor: weekdayBackgroundColor[ 6 ] },
-	cell: {
-		borderWidth: '0.375rem',
-		borderStyle: 'solid',
-		borderColor: 'black',
-		verticalAlign: 'middle',
-		position: 'relative',
-	},
-	cellInner: {
-		paddingTop: '1rem',
-		paddingLeft: '0.66rem',
-	},
-	cellDayDate: {
-		extend: 'cell',
-
-		borderRightWidth: 0,
-		paddingRight: 0,
-		textAlign: 'right',
-	},
-	cellDayName: {
-		extend: 'cell',
-
-		borderLeftWidth: 0,
-		paddingLeft: '0.66rem',
-	},
+	day: {},
 });
 
-function Day({ date, numberOfTimeSlots }) {
+function Day({ date, numberOfTimeSlots, isPlaceholder }) {
 	const classes = useStyles();
 	const { t } = useTranslation();
 
-	// generate cells for each option one
-	const cells = [];
-	for( let i = 0; i < numberOfTimeSlots; i++ ){
-		cells.push(
-			<td
-				key={ date.format( 'YYYY-MM-DD' ) + `_${ i }` }
-				className={ classes.cell }>
-				<Background
-					backgroundColor={ weekdayBackgroundColor[ date.day() ] }>
-					&nbsp;
-				</Background>
-			</td>
+	const weekday = date.day();
+
+	if( isPlaceholder ) {
+		return (
+			<DateRow
+				classNames={ classes.day }
+				day='&nbsp;'
+				dayName='&nbsp;'
+				style={ DateRow.styles.invisible }
+				numberOfTimeSlots={ numberOfTimeSlots } />
 		);
 	}
 
-	const weekendHighlight = {};
-	weekendHighlight[ classes.saturday ] = date.day() === 6;
-	weekendHighlight[ classes.sunday ]   = date.day() === 0;
-
 	return (
-		<tr className={ classNames( classes.rowDay, weekendHighlight ) }>
-
-			<td className={ classes.cellDayDate }>
-				<Background
-					className={ classes.cellInner }
-					backgroundColor={ weekdayBackgroundColor[ date.day() ] } >
-					{ date.format( 'DD' ) }.
-				</Background>
-			</td>
-
-			<td className={ classes.cellDayName }>
-				<Background
-					backgroundColor={ weekdayBackgroundColor[ date.day() ] }
-					className={ classes.cellInner }>
-					{ t( 'day_names.' + date.day() + '.abbrev' ) }
-				</Background>
-			</td>
-
-			{ cells }
-
-		</tr>
+		<DateRow
+			classNames={ classes.day }
+			day={ date.format( 'DD.' ) }
+			dayName={ t( 'day_names.' + weekday + '.abbrev' ) }
+			style={ weekdayStyle[ weekday ] }
+			numberOfTimeSlots={ numberOfTimeSlots } />
 	);
 }
 
@@ -105,4 +54,3 @@ Day.propTypes = {
 }
 
 export default Day;
-
